e63854f6934470cd27dd36d03539e92d4cea3c1e
Ulisses Roque <ulisses.roque@gmail.com>
2022-10-23 23:08:12 -0300

    test: implements test for read_brazilian_file function

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,12 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    brazilian_jobs = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    dict_mock = {
+        'title': 'Maquinista',
+        'salary': '2000',
+        'type': 'trainee'
+    }
+
+    assert dict_mock in brazilian_jobs
